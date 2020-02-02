fdef1dff0a5499a7dbb557c52a2463fda2dfc8b0
kibebr <0763722@students.dadeschools.net>
2020-02-01 23:04:13 -0500

    Added first exercise

---
+++ exercises/fundamentals-part1.js
@@ -0,0 +1,23 @@
+// Add 2 numbers together! (just type console.log(23 + 97) into your html file)
+let sum = 23 + 97;
+
+// Add a sequence of 6 different numbers together.
+let sequence = 23 + 97 + 1 + 4 + 5 + 2;
+
+// Print the solution to the following equation: (4 + 6 + 9) / 77
+console.log((4 + 6 + 9) / 77);
+
+// Let’s use variables! (several instructions)
+let a = 10;
+console.log(a);
+a = 9 * a;
+let b = 7 * a;
+console.log(b);
+
+// You should be getting the hang of this by now… try this sequence: (several instructions)
+const max = 57;
+let actual = max - 13;
+let percentage = actual / max;
+console.log(percentage);
+
+
