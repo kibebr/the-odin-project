4c70a4c904142ff165a52e43ab211b96b1a57e2e
kibe <47016873+kibebr@users.noreply.github.com>
2020-03-10 23:03:12 -0400

    Update function1.js

--- exercises/mdn/test-your-skills-functions/function1.js
+++ exercises/mdn/test-your-skills-functions/function1.js
@@ -1,3 +1,7 @@
+// For the first task, you have to create a simple function that prints a random name from the provided array (names) to the provided paragraph (para), and then run it once.
+// Try updating the live code below to recreate the finished example:
+
+
 let names = ['Chris', 'Li Kang', 'Anne', 'Francesca', 'Mustafa', 'Tina', 'Bert', 'Jada']
 let para = document.createElement('p');
 
