c474d0d25cd8789132f86f98580d3fe6e9608271
kibe <0763722@students.dadeschools.net>
2020-03-23 01:39:31 -0400

    Added 'calculator' exercise

---
+++ exercises/calculator/calculator.js
@@ -0,0 +1,46 @@
+function add(num1,num2){
+	return num1+num2;
+}
+
+function subtract(num1, num2){
+	return num1-num2;
+}
+
+function sum(...numbers){
+	let total = 0;
+	numbers.forEach(number => {
+		total += number;
+	});
+	return total;
+}
+
+function multiply(...numbers){
+	let total = 0;
+	numbers.forEach(number => {
+		total *= number;
+	});
+	return total; 
+}
+
+function power(base, exp){
+	let result = 0;
+	for(let i = 0; i < exp; ++i)
+		result *= base;
+	return result;
+}
+
+function factorial(number){
+	let result = number;
+	while(number != 1){
+		result *= (--number);
+	}
+}
+
+module.exports = {
+	add,
+	subtract,
+	sum,
+	multiply,
+	power,
+        factorial
+}
